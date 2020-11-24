37feeead03d7c947daec17e7f8cbac8aa83e9124
Mar-Nb <63231050+Mar-Nb@users.noreply.github.com>
2020-11-24 16:23:08 +0100

    Update Greeter.java
    Modification du commentaire Javadoc de la fonction greet()

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,11 +13,10 @@ public class Greeter {
   }
 
   /**
-   * public final String greet(String)
    * Fonction qui salue une personne dont le nom est passé en paramètre.
    * 
-   * @param String someone - Le nom de la personne saluée.
-   * @return String - Une phrase de salutation
+   * @param someone (String) - Le nom de la personne saluée.
+   * @return Une phrase de salutation (String)
    */
   public final String greet(final String someone) {
     return String.format("Hello, Martin %s!", someone);
